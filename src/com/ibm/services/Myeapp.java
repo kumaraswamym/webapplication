05c7b80c416fa1ee29bac932c9012564cae17df7
Kumaraswamy Marusani <kumar211.m@gmail.com>
2017-09-09 06:42:09 +0200

    this is myeapp java class

---
+++ src/com/ibm/services/Myeapp.java
@@ -0,0 +1,11 @@
+package com.ibm.services;
+
+public class Myeapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Devops");
+
+	}
+
+}
